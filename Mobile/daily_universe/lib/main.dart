ab6e2a5ab20d9d4af44e54c1939084046782aaa0
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-04-11 15:13:11 +0300

    Сделал поле чекбокса

--- Mobile/daily_universe/lib/main.dart
+++ Mobile/daily_universe/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main(){
   runApp(const MyApp());
 }
@@ -38,70 +39,56 @@ class Home extends StatefulWidget{
 class _HomeState extends State<Home> {
   final Map<String, bool> _map = {};
   int _count = 0;
+
+  List<Widget> checkRow = [];
   
   @override
   Widget build(BuildContext context) {
+    checkRow = [
+      Container(
+        child: Row(children: [
+            Expanded(
+              child: Column(children: [
+                Container(
+                  child: Checkbox(
+                    value: false,
+                    onChanged: (bool? value)=> setState(() => {})
+                  ),
+                  color: Colors.blue,
+                  padding: const EdgeInsets.only(left: 3, right: 3, top: 3, bottom: 3),
+                ), 
+              ]),
+              flex: 1,
+            ),
+            Expanded(
+              child: Column(children: const [
+                    Text('Checkbox1', textAlign: TextAlign.start),
+              ]),
+              flex: 15,
+            ),
+            Expanded(
+              child: Column(children: [
+                Container(child: IconButton(
+                  icon: const Icon(Icons.highlight_remove),
+                  onPressed: () {  },),
+                  color: Colors.red,
+                  margin: const EdgeInsets.only(top:5, bottom: 5),
+                ),
+              ]),
+              flex: 1,
+            )
+          ],),
+          color: Colors.grey,
+          margin: const EdgeInsets.only(left: 5, right: 5, top: 5, bottom: 5),
+        )
+      ];
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () => setState(() => _map.addEntries([MapEntry('Checkbox #${++_count}', false)])),
       ),
-      body: SingleChildScrollView( child: Row(children: [
-        SizedBox(
+      body: SingleChildScrollView(child: Column(children: checkRow)),
 
-        child: Column(children: [
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox1'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox2'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox3'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox4'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox5'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox6'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox7'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox8'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox9'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox10'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox11'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox12'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox13'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox14'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox15'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox16'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox17'), controlAffinity: ListTileControlAffinity.leading,),
-          CheckboxListTile(value: false, onChanged: (bool? value)=> setState(() => {}), title: const Text('Checkbox18'), controlAffinity: ListTileControlAffinity.leading,),
-        ]),
-        width: MediaQuery.of(context).size.width * 0.9,
-        ),
-        SizedBox(
-        child: Column(children: [
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-          SizedBox(child: IconButton(icon: Icon(Icons.highlight_remove), onPressed: () {  },), height: 48,),
-        ]),
-        width: MediaQuery.of(context).size.width * 0.1,
-        )
-        // Column(
-        // children: [
-        //   Icon(Icons.delete),
-        //   Icon(Icons.delete),
-        //   Icon(Icons.delete),
-        // ]),
-      ],
-      ))
     );
-  }
+  }  
 }
