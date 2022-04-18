095ffa12b729baafba4ee104b88954e2f0fa6f0e
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-04-18 17:01:34 +0300

    Stable, but no as expected

--- Mobile/daily_universe/lib/main.dart
+++ Mobile/daily_universe/lib/main.dart
@@ -48,11 +48,46 @@ class _HomeState extends State<Home> {
           SliverFillRemaining(
             hasScrollBody: false,
             child: Column(
-              children: checkboxesList.values.toList()
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  width: MediaQuery.of(context).size.width - 20,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(1, 3),
+                      ),
+                    ],
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          checkboxesList.remove(counter--);
+                        }); 
+                      },
+                      child: Container( margin: EdgeInsets.only(left: 160, top: 30), width: 200, height: 40, child: Text("Удалить"))
+                    )
+                  )
+                )
+              ] + checkboxesList.values.toList()
             )
           )
         ]
-      )
+      ),
+      
     );
   }
 }
