d612a252a727e0d53e0d7969798ec9d909cb510f
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-05-09 13:01:28 +0300

    Добавил переходы между окнами

--- Web/login/script.js
+++ Web/login/script.js
@@ -5,22 +5,25 @@ const button = document.querySelector(".footer__text-bottom");
 button.addEventListener("click", event => {
     if (localStorage.users == undefined) {
         alert("Пройдите регистрацию!");
-    } else {
+    } 
+    else {
         let users = JSON.parse(localStorage.users);
         let length = users.name.length;
         if (length == 0) {
             alert("Пройдите регистрацию!");
-        } else {
+        }
+        else {
             for (let i = 0; i < length; i++) {
                 if (users.email[i] == inputEmail.value && users.password[i] == inputPassword.value) {
                     localStorage.activeUserIndex = i;
-                    alert("Вы успешно вошли в свой аккаунт!");
+                    window.location.replace('../main/index.html');
                     break;
                 }
             }
             if (localStorage.activeUserIndex == "unknown") {
                 alert("Вы ввели неверные данные или не заполнили все поля!");
-            } else {
+            }
+            else {
                 if (users.email[localStorage.activeUserIndex] != inputEmail.value ||
                     users.password[localStorage.activeUserIndex] != inputPassword.value) {
                         alert("Вы ввели неверные данные или не заполнили все поля!");
