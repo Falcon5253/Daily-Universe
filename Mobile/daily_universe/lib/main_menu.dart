0ace9f1a82354e243d9d6c9a06c9daea39501cc1
mr.sokol5253 <mr.sokol5253@gmail.com>
2022-05-15 19:09:37 +0300

    Доделал верстку основного окна

--- Mobile/daily_universe/lib/main_menu.dart
+++ Mobile/daily_universe/lib/main_menu.dart
@@ -13,48 +13,316 @@ class MainMenu extends StatefulWidget {
 
 class _MainMenuState extends State<MainMenu> {
   int _selectedIndex = 0;
-
-   void _onItemTapped(int index) {
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:   Container(
+      body: Container(
         child: Column(
           children: [
-            Row(
-              children: [Text("1")],
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  bottom: BorderSide(width: 5, color: Colors.black)
+                )
+              ),
+              padding: const EdgeInsets.only(top: 25),
+              width: MediaQuery.of(context).size.width,
+              height: 185,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        height: 45,
+                        child: Row(children: [
+                          SizedBox(
+                            width: 45,
+                            height: 45,
+                            child: InkWell(
+                              onTap: () { 
+                                Navigator.pushNamed(context, '/reg');
+                              },
+                              child: Icon(Icons.settings, size: 40,),
+                            )
+                          ),
+                          const SizedBox(
+                            width: 45,
+                            height: 45,
+                            child: InkWell(
+                              child: Icon(Icons.headset_mic, size: 40,),
+                            )
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width-180,
+                          ),
+                          const SizedBox(
+                            width: 45,
+                            height: 45,
+                            child: InkWell(
+                              child: Icon(Icons.calendar_today, size: 40,),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 45,
+                            height: 45,
+                            child: Icon(Icons.account_box, size: 47,),
+                          )
+
+                        ]),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 45,
+                        child: Row(
+                          children: [
+                            Container(
+                              width: (MediaQuery.of(context).size.width/2) - MediaQuery.of(context).size.width/4,
+                              padding: const EdgeInsets.only(left: 10),
+                              child: const Text("Май", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width/2,
+                              child: InkWell (
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(25),
+                                    color: Colors.white,
+                                    boxShadow: const [
+                                      BoxShadow(color: Colors.black, spreadRadius: 3),
+                                    ],
+                                  ),
+                                  child: const Center(
+                                    child: Text("Понедельник, 16 мая", style: TextStyle(fontWeight: FontWeight.w900, fontSize: 18), textAlign: TextAlign.center,)
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: (MediaQuery.of(context).size.width/2) - MediaQuery.of(context).size.width/4,
+                              padding: const EdgeInsets.only(right: 10),
+                              child: const Text("2022 г.", textAlign: TextAlign.right, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container (
+                        margin: const EdgeInsets.only(top: 15),
+                        width: MediaQuery.of(context).size.width,
+                        child: Row(children: [
+                          Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.green,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Пн"),
+                                Text("01")
+                              ],)
+                            ),
+                          ),
+                                              Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Вт"),
+                                Text("02")
+                              ],)
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Ср"),
+                                Text("03")
+                              ],)
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Чт"),
+                                Text("04")
+                              ],)
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Пт"),
+                                Text("05")
+                              ],)
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Сб"),
+                                Text("06")
+                              ],)
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            width: (MediaQuery.of(context).size.width-80)/7,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(color: Colors.black, spreadRadius: 3),
+                              ],
+                            ),
+                            child: InkWell(
+                              child: Column(children: const [
+                                Text("Вс"),
+                                Text("07")
+                              ],)
+                            ),
+                          ),
+                        ]),  
+                      ),
+                    ]
+                  )
+                ],
+              ),
             ),
-            Row(
-              children: [Text("2")],
-            ),
-            Row(
-              children: [Text("3")],
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height-245,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(color: Colors.black, spreadRadius: 5),
+                  ],
+                ),
+              child: Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width-20,
+                        margin: const EdgeInsets.only(top: 10),
+                        height: 200,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width-20,
+                        margin: const EdgeInsets.only(top: 10),
+                        height: 200,
+                        color: Colors.yellow,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width-20,
+                        margin: const EdgeInsets.only(top: 10),
+                        height: 200,
+                        color: Colors.grey,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width-20,
+                        margin: const EdgeInsets.only(top: 10),
+                        height: 200,
+                        color: Colors.purple,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width-20,
+                        margin: const EdgeInsets.only(top: 10),
+                        height: 200,
+                        color: Colors.cyan,
+                      )
+                    ]
+                  ),
+                )
+              )
             )
-          ],
+          ]
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.sports),
+            label: 'Спорт',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.local_drink),
+            label: 'Отдых',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.backpack),
+            label: 'Работа',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank),
+            label: 'Еда',
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
       ),
     );
